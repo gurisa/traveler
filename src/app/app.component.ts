e7f29f39f75e6e1a5c6daa19d04ebc10910bb79c
Raka Suryaardi Widjaja <raka.suryadi@gmail.com>
2018-04-12 01:34:55 +0700

    adding angular material fw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Traveler';
 }
