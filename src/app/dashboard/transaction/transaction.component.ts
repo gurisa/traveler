ab14978611c38fc365c29660ffb4256c56e71e6c
Raka Suryaardi Widjaja <raka.suryadi@gmail.com>
2018-06-25 18:10:20 +0700

    [ADD] transaction adjusment

--- src/app/dashboard/transaction/transaction.component.ts
+++ src/app/dashboard/transaction/transaction.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { Router } from '@angular/router';
+import { TransactionService } from './../../service/transaction.service';
+
 
 @Component({
   selector: 'app-transaction',
@@ -6,10 +10,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transaction.component.scss']
 })
 export class DashboardTransactionComponent implements OnInit {
+  
+  private transactions = [];
 
-  constructor() { }
+  constructor(
+    private http: Http,
+    private router: Router,
+    private TransactionService: TransactionService
+  ) { 
+
+  }
 
   ngOnInit() {
+    this.gets();
+  }
+
+  gets() {
+    this.TransactionService.gets()
+    .subscribe(response => {
+      if (response && response.status) {
+        this.transactions = response.data;
+      }     
+    });
+  }
+
+  paid(id) {    
+    if (id) {
+      if (confirm('Mark this transaction as done?')) {
+        this.TransactionService.paid(id)
+        .subscribe(response => {
+          if (response && response.status) {            
+            this.gets();
+          }          
+        },
+        error => {
+          alert('failed mark transaction');
+        });
+      }   
+    }
+    else {
+      alert('error occur');
+    }
+  }
+
+  unpaid(id) {
+    if (id) {
+      if (confirm('Mark this transaction as undone?')) {
+        this.TransactionService.unpaid(id)
+        .subscribe(response => {
+          if (response && response.status) {
+            this.gets();
+          }          
+        },
+        error => {
+          alert('failed mark transaction');
+        });
+      }   
+    }
+    else {
+      alert('error occur');
+    }
   }
 
 }
--- src/app/service/transaction.service.ts
+++ src/app/service/transaction.service.ts
@@ -1,8 +1,64 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class TransactionService {
 
-  constructor() { }
+  constructor(
+    private http: Http
+  ) {
+    
+  }
+
+    
+  get(id) {
+    return this.http.get('http://traveler.local/api/v0/transactions/' + id)
+    .map(response => {
+      return response.json();   
+    });   
+  }
+  
+  gets() {
+    return this.http.get('http://traveler.local/api/v0/transactions')
+    .map(response => {
+      return response.json();    
+    });
+  }
+
+  update(id, transaction) {
+    return this.http.patch('http://traveler.local/api/v0/transactions/' + id, transaction)
+      .map(response => {
+          return response.json();
+      });
+  }
+
+  paid(id) {
+    return this.http.post('http://traveler.local/api/v0/transactions/' + id + '/paid', {})
+      .map(response => {
+          return response.json();
+      });
+  }
+
+  unpaid(id) {
+    return this.http.post('http://traveler.local/api/v0/transactions/' + id + '/unpaid', {})
+      .map(response => {
+          return response.json();
+      });
+  }
+
+  store(transaction) {
+    return this.http.post('http://traveler.local/api/v0/transactions', transaction)
+    .map(response => {
+      return response.json();
+    });
+  }
+
+  delete(id) {
+    return this.http.delete('http://traveler.local/api/v0/transactions/' + id)
+    .map(response => {
+      return response.json();
+    });
+  }
 
 }
