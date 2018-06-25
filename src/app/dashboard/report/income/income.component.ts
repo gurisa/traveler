971f9c80c9be31344712ce49e3d9116e21ec973a
Raka Suryaardi Widjaja <raka.suryadi@gmail.com>
2018-06-25 19:14:44 +0700

    [ADD] chartjs and init

--- src/app/dashboard/report/income/income.component.ts
+++ src/app/dashboard/report/income/income.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-income',
@@ -7,9 +8,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardReportIncomeComponent implements OnInit {
 
-  constructor() { }
+  private incomeReport;
 
-  ngOnInit() {
+  constructor(
+
+  ) { 
+
   }
 
+  ngOnInit() {
+    this.setChart("income-report");
+  }
+
+  setChart(identifier) {
+    var ctx = document.getElementById(identifier);
+    this.incomeReport = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Jan", "Feb", "Mar", "Apr", "Mar", "Mei"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+  }
+
+  get() {
+
+  }
 }
--- src/app/dashboard/report/inventory/inventory.component.ts
+++ src/app/dashboard/report/inventory/inventory.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-inventory',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardReportInventoryComponent implements OnInit {
 
-  constructor() { }
+  private inventoryReport;
+
+  constructor() {
+
+  }
 
   ngOnInit() {
+    this.setChart("inventory-report");
+  }
+
+  setChart(identifier) {
+    var ctx = document.getElementById(identifier);
+    this.inventoryReport = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+          datasets: [{
+            data: [10, 20, 30, 25],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+            ],
+            borderWidth: 1
+          }],
+      
+          labels: [
+              'Bus',
+              'Plane',
+              'Train',
+              'Car'
+          ]
+        },
+        options: {
+            // scales: {
+            //     yAxes: [{
+            //         ticks: {
+            //             beginAtZero:true
+            //         }
+            //     }]
+            // }
+        }
+    });
   }
 
 }
