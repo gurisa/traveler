ee6d1cf6dabea50e2b9650ce17e9722f470dcde9
Raka Suryaardi Widjaja <raka.suryadi@gmail.com>
2018-07-17 22:42:44 +0700

    [PUBLISH]

--- src/app/app-setting.service.ts
+++ src/app/app-setting.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AppSetting {
 
-  public static API = 'http://traveler.local/api/v0'; 
+  public static API = 'https://tr4veler.gurisa.com/api/v0'; 
 
   constructor() {
     // this.API = (isDevMode) ? 'http://traveler.local/api/v0' : 'https://tr4veler.gurisa.com/api/v0';
